e63530e431e7ce11b0da062cebce49aed3ce8c17
ComanGames <alfaa.gen@gmail.com>
2016-04-15 19:45:59 +0300

    Last changes

--- LessonWithSashaLoops/Program.cs
+++ LessonWithSashaLoops/Program.cs
@@ -10,8 +10,14 @@ namespace LessonWithSashaLoops
     {
         static void Main(string[] args)
         {
+            //Array Initialization first way
             int[] A = new[] {5, 10, 25,8};
+            //Array Initialization second way
             int[] B = new int[3];
+            //Array intialization last way
+            int[] C;
+            C = new int[5];
+
             //{5,10,25}
             Console.WriteLine("Array A");
             Console.WriteLine($"A[0] ={A[0]}");
