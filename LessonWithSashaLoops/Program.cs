60b1d2b4365dee5a79412e6aa0f4a720992210b8
ComanGames <alfaa.gen@gmail.com>
2016-04-15 20:04:30 +0300

    Char to string to char

--- LessonWithSashaLoops/Program.cs
+++ LessonWithSashaLoops/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +11,52 @@ namespace LessonWithSashaLoops
     {
         static void Main(string[] args)
         {
-            //Array Initialization first way
-            int[] A = new[] {5, 10, 25,8};
+//            FirstPartOfLesson();
+//            SecondPartOfLesson();
+
+            //String to char array  
+            string word = "Hello Sasha";
+            char[] c = word.ToCharArray();
+            for (int i = 0; i < c.Length; i++)
+                Console.WriteLine(c[i]);
+            c[5] = 'x';
+
+            //Char array to string
+            word = new string(c);
+            Console.WriteLine(word);
+
+            Console.ReadKey();
+
+        
+        }
+
+        private static void SecondPartOfLesson()
+        {
+            while (true)
+            {
+                int[] A = new int[3];
+                Console.WriteLine(int.MaxValue);
+                Console.WriteLine("Write A[0]");
+                A[0] = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Write A[1]");
+                A[1] = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Write A[2]");
+                A[2] = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine($"A[0] ={A[0]}");
+                Console.WriteLine($"A[1] ={A[1]}");
+                Console.WriteLine($"A[2] ={A[2]}");
+                Console.WriteLine($"A[0]+A[1]+A[2]={A[0] + A[1] + A[2]}");
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        private static void FirstPartOfLesson()
+        {
+//Array Initialization first way
+            int[] A = new[] {5, 10, 25, 8};
             //Array Initialization second way
             int[] B = new int[3];
             //Array intialization last way
@@ -33,10 +78,6 @@ namespace LessonWithSashaLoops
             Console.WriteLine("Legth of Arrays");
             Console.WriteLine($"A.Length ={A.Length}");
             Console.WriteLine($"B.Length ={B.Length}");
-
-
-            Console.ReadKey();
-
         }
     }
 }
